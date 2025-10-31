2f53abd41d3b668d48643c7891ec9145b24f56e6
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-11-01 03:44:21 +0530

    Update API endpoint for editing user profile

--- frontend/src/components/EditProfile.jsx
+++ frontend/src/components/EditProfile.jsx
@@ -43,7 +43,7 @@ const EditProfile = () => {
     try {
       setLoading(true);
       const res = await axios.post(
-        "http://localhost:8000/api/v1/user/profile/edit",
+        "https://social-media-msj4.onrender.com/api/v1/user/profile/edit",
         formData,
         {
           headers: {
